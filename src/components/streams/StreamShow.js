9896d16d1ff15990a513e508b184e7ee7239109d
Emanuel <emanuel.beni@gmail.com>
2020-03-06 22:47:31 +0700

    Added Node-Media-Server Functionality

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,10 +1,38 @@
 import { fetchStream } from "../../actions"
 import React, { Component } from "react"
 import { connect } from "react-redux"
+import flv from "flv.js"
 
 export class StreamShow extends Component {
+	constructor(props) {
+		super(props)
+
+		this.videoRef = React.createRef()
+	}
+
 	componentDidMount = () => {
-		this.props.fetchStream(this.props.match.params.id)
+		const { id } = this.props.match.params
+		this.props.fetchStream(id)
+		this.buildPlayer()
+	}
+
+	componentDidUpdate() {
+		this.buildPlayer()
+	}
+
+	buildPlayer() {
+		if (this.player || !this.props.stream) {
+			return
+		}
+		const { id } = this.props.match.params
+
+		//Setting RTMP Server using Node-Server-Media
+		this.player = flv.createPlayer({
+			type: "flv",
+			url: `http://localhost:8000/live/${id}.flv`
+		})
+		this.player.attachMediaElement(this.videoRef.current)
+		this.player.load()
 	}
 
 	render() {
@@ -13,7 +41,8 @@ export class StreamShow extends Component {
 		}
 		const { title, description } = this.props.stream
 		return (
-			<div className='container'>
+			<div className='container mt-2'>
+				<video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
 				<h2>{title}</h2>
 				<h5>{description}</h5>
 			</div>
